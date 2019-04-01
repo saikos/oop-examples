cc7fe99a427e9705755330b2b46b10f4ae53ef7f
Kostas Saidis <saikos@gmail.com>
2019-04-01 20:11:42 +0300

    Add streams and lambdas example

--- src/org/afdemp/cb6/ex09/Main.java
+++ src/org/afdemp/cb6/ex09/Main.java
@@ -1,23 +1,41 @@
 package org.afdemp.cb6.ex09;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.afdemp.cb6.ex02.Printer;
 import org.afdemp.cb6.ex02.PrinterNotEnabledException;
 import org.afdemp.cb6.ex03.calc.ArithmeticCalculation;
 
 /*
- Examples of anonymous inner classes.
+ Examples of anonymous inner classes, lambdas and streams.
 */
 public class Main {
-
-    public static void main(String[] args) {
         
+    public static void main(String[] args) {    
         String[] names = new String[]{
             "Tom",
             "John",
             "Robert"
         };
+        
+        Arrays.sort(names, (String s1, String s2) -> {
+            int l1 = s1.length();
+            int l2 = s2.length();
+
+            if (l1 < l2) {
+                return -1;
+            }
+
+            if (l1 > l2) {
+                return 1;
+            }
+
+            return 0;                
+        });
                 
         Arrays.sort(names, new Comparator<String>() {
             @Override
@@ -40,6 +58,8 @@ public class Main {
         
         System.out.println(Arrays.asList(names));               
        
+        streamExampleOld();
+        streamExample();    
         
         ArithmeticCalculation mult = 
                 new ArithmeticCalculation("Mult", 1.0, 2.0) {
@@ -60,9 +80,44 @@ public class Main {
                 throw new PrinterNotEnabledException("NOT");
             }            
         };
-        p.print("Koukou");                
-        
-        
+        p.print("Koukou");
     }
+    
+    public static void streamExample() {
+        List<String> newList = Stream.of("Cake", "Alpha", "Beta", "Coco", "Dear").                
+                filter( s -> s.startsWith("C") ).
+                map(s -> s.toLowerCase()).
+                collect(Collectors.toList());
+        System.out.println(newList);
+    }
+
+    
+    public static void streamExampleOld() {
+        List<String> strings = Arrays.asList("A", "B", "C", "D");
+        System.out.println(
+            allItemsToLowerCase(
+                allItemsStartingWith("C", strings)
+            )
+        );        
+    }    
+    
+    public static List<String> allItemsStartingWith(String prefix, List<String> list) {
+        List<String> newList = new ArrayList<>();
+        for(String s: list) {
+            if (s.startsWith(prefix)) {
+                newList.add(s);
+            }
+        }
+        return newList;
+    }
+    
+    public static List<String> allItemsToLowerCase(List<String> list) {
+        List<String> newList = new ArrayList<>();
+        for(String s: list) {            
+            newList.add(s.toLowerCase());            
+        }
+        return newList;
+    }
+
     
 }
